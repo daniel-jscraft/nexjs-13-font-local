af1127a904028a3cd18ea38ef07a165fe25f8175
Daniel <daniel.nastase@gmail.com>
2022-11-29 16:37:27 +0200

    ready

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,11 @@
-import {Roboto} from '@next/font/google'
-
-const roboto = Roboto({
-  weight : ['300']
-})
-
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body className={roboto.className}>{children}</body>
+      <body style={{
+        color: 'white',
+        backgroundColor: '#3f69c7',
+      }}>{children}</body>
     </html>
   )
 }
